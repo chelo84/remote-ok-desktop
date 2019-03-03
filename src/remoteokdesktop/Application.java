6c175025f11e2ab9941a5fda2a5480aeacaea0b6
André <bls.andre.agostinho@gmail.com>
2019-03-03 20:01:18 -0300

    implemented function in systemtray

--- src/remoteokdesktop/Application.java
+++ src/remoteokdesktop/Application.java
@@ -4,6 +4,9 @@ import remoteokdesktop.gui.ListFrame;
 import remoteokdesktop.gui.LoginFrame;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class Application {
 
@@ -14,12 +17,36 @@ public class Application {
             if (SystemTray.isSupported()) {
                 SystemTray tray = SystemTray.getSystemTray();
                 Image image = Toolkit.getDefaultToolkit().getImage("imagem.jpeg");
+                
                 PopupMenu popup = new PopupMenu();
-                MenuItem item = new MenuItem("Um Menu Item");
-                popup.add(item);
-                TrayIcon trayIcon = new TrayIcon(image, "Remote OK", popup);
-                trayIcon.setImageAutoSize(true);
+                
 
+    
+    //ação de executar
+    ActionListener actionListener = new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+    };
+    //ação de sair
+     ActionListener sairListener = new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    System.exit(0);
+    }
+    };     
+    //Opção executar
+     MenuItem executarItem = new MenuItem("Abrir");
+     executarItem.addActionListener(actionListener);
+     popup.add(executarItem);
+     TrayIcon trayIcon = new TrayIcon(image, "Remote OK", popup);
+     trayIcon.setImageAutoSize(true);
+    //Opção sair
+     MenuItem sairItem = new MenuItem("Sair");
+     sairItem.addActionListener(sairListener);
+     popup.add(sairItem);               
+
+            
                 try {
                     tray.add(trayIcon);
                 } catch (AWTException e) {
